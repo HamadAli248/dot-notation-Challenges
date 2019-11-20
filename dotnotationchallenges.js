2baa78a51f2133a8d2a02cb6bcfecdd466d6ded0
Hamad Ali <hammadali248@gmail.com>
2019-11-20 17:07:43 +0000

    dot-notation challenges

---
+++ dotnotationchallenges.js
@@ -0,0 +1,18 @@
+/* challenge 1. JS Dot Notation
+Search on each of the below string methods. Make an example of each to demonstrate your understanding.
+search()
+lastIndexOf()
+slice()
+substring(start, end)
+substr(start, length)
+replace()
+concat()
+charAt()
+*/
+
+console.log(5)
+console.log("Hello")
+console.log("Hello".replace(/l/g,"w"))
+console.log("hello".charAt(0))
+console.log("hello".includes("e"))
+console.log("hello".indexOf("e"))
